ce99f337429ec9f8c008549e7f456fd389d395cd
AniSargsyan97 <ani97sargsyan@gmail.com>
2025-05-06 16:14:20 +0400

    coordination of menu detailս

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Test task=</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
@@ -61,26 +61,6 @@
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
-
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Համոզված եք,որ ցանկանում եք լքել էջը?</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Չգիտեմ</button>
-                <a class="btn btn-primary" href="{{url('admin/login')}}">Այո</a>
-            </div>
-        </div>
-    </div>
-</div>
 
 <!-- Bootstrap core JavaScript-->
 {{--<script src="{{asset('admin/vendor/jquery/jquery.min.js')}}"></script>--}}
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,42 +1,4 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-    {{--    @include('admin.layout.menu_items')--}}
-{{--    @php--}}
-{{--        $menu_items = [--}}
-
-{{--             [--}}
-{{--                'url' => 'product_list',--}}
-{{--                'name' => 'product_list',--}}
-{{--                'label' => 'Products',--}}
-{{--                'params' => [0],--}}
-{{--                'icon' => 'fas fa-couch'--}}
-{{--            ],--}}
-{{--            [--}}
-{{--                'url' => 'product_type',--}}
-{{--                'name' => 'prod_type_list',--}}
-{{--                'label' => 'Orders',--}}
-{{--                'params' => null,--}}
-{{--                'icon' => 'fas fa-clipboard'--}}
-{{--            ],--}}
-{{--            [--}}
-{{--                'url' => 'companies_list',--}}
-{{--                'name' => 'company_list',--}}
-{{--                'label' => 'Order_items',--}}
-{{--                'params' => null,--}}
-{{--                'role' => 2,--}}
-{{--                'icon' => 'fas fa-briefcase'--}}
-{{--            ],--}}
-{{--            [--}}
-{{--                'url' => 'shop_list',--}}
-{{--                'name' => 'market_list',--}}
-{{--                'label' => 'Warehouses',--}}
-{{--                'params' => [0],--}}
-{{--                'role' => 3,--}}
-{{--                'icon' => 'fas fa-store'--}}
-{{--            ],--}}
-
-{{--    ];--}}
-{{--        $page = Session::get('page');--}}
-{{--    @endphp--}}
 
     <a class="sidebar-brand d-flex align-items-center justify-content-center" style="line-height: 1.5"
        href="">
@@ -47,121 +9,27 @@
     </a>
 
     <hr class="sidebar-divider my-0">
-        <li class="nav-item menu_active_item">
+        <li class="nav-item {{Route::is('products') ? 'active' : '' }}">
             <a class="nav-link"
-               href="#">
+               href="{{route('products')}}">
                 <i class="fas fa-store"></i>
                 <span>Products</span>
             </a>
         </li>
-        <li class="nav-item menu_active_item">
+        <li class="nav-item {{Route::is('orders') ? 'active' : '' }}">
             <a class="nav-link"
-               href="#">
+               href="{{route('orders')}}">
                 <i class="fas fa-clipboard"></i>
                 <span>Orders</span>
             </a>
         </li>
-        <li class="nav-item menu_active_item">
+        <li class="nav-item {{Route::is('warehouses') ? 'active' : '' }}">
             <a class="nav-link"
-               href="#">
-                <i class="fas fa-file"></i>
-                <span>Order_items</span>
-            </a>
-        </li>
-        <li class="nav-item menu_active_item">
-            <a class="nav-link"
-               href="#">
+               href="{{route('warehouses')}}">
                 <i class="fas fa-warehouse"></i>
                 <span>Warehouses</span>
             </a>
         </li>
-        <li class="nav-item menu_active_item">
-            <a class="nav-link"
-               href="#">
-                <i class="fas fa-business-time"></i>
-                <span>Stocks</span>
-            </a>
-        </li>
-
-
-{{--        <li class="nav-item">--}}
-{{--            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"--}}
-{{--               aria-expanded="true" aria-controls="collapseTwo">--}}
-{{--                <i class="fas fa-fw fa-cog"></i>--}}
-{{--                <span>Settings</span>--}}
-{{--            </a>--}}
-{{--            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">--}}
-{{--                <div class="bg-white py-2 collapse-inner rounded">--}}
-{{--                    <h6 class="collapse-header">Basic Settings</h6>--}}
-{{--                    <a class="collapse-item nav-link set_class" href="{{url('admin/update-password')}}">Change password</a>--}}
-{{--                    --}}{{--                <a class="collapse-item nav-link set_class" href="{{url('admin/update-admin-details')}}">Ադմինի--}}
-{{--                    --}}{{--                    տվյալների <br>թարմացում</a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-
-{{--        </li>--}}
-
-{{--    <!-- Nav Item - Utilities Collapse Menu -->--}}
-{{--    <li class="nav-item">--}}
-{{--        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"--}}
-{{--           aria-expanded="true" aria-controls="collapseUtilities">--}}
-{{--            <i class="fas fa-fw fa-wrench"></i>--}}
-{{--            <span>Utilities</span>--}}
-{{--        </a>--}}
-{{--        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"--}}
-{{--             data-parent="#accordionSidebar">--}}
-{{--            <div class="bg-white py-2 collapse-inner rounded">--}}
-{{--                <h6 class="collapse-header">Custom Utilities:</h6>--}}
-{{--                <a class="collapse-item" href="utilities-color.html">Colors</a>--}}
-{{--                <a class="collapse-item" href="utilities-border.html">Borders</a>--}}
-{{--                <a class="collapse-item" href="utilities-animation.html">Animations</a>--}}
-{{--                <a class="collapse-item" href="utilities-other.html">Other</a>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </li>--}}
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-{{--    <li class="nav-item">--}}
-{{--        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"--}}
-{{--           aria-expanded="true" aria-controls="collapsePages">--}}
-{{--            <i class="fas fa-fw fa-folder"></i>--}}
-{{--            <span>Pages</span>--}}
-{{--        </a>--}}
-{{--        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">--}}
-{{--            <div class="bg-white py-2 collapse-inner rounded">--}}
-{{--                <h6 class="collapse-header">Login Screens:</h6>--}}
-{{--                <a class="collapse-item" href="login.html">Login</a>--}}
-{{--                <a class="collapse-item" href="register.html">Register</a>--}}
-{{--                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>--}}
-{{--                <div class="collapse-divider"></div>--}}
-{{--                <h6 class="collapse-header">Other Pages:</h6>--}}
-{{--                <a class="collapse-item" href="404.html">404 Page</a>--}}
-{{--                <a class="collapse-item" href="blank.html">Blank Page</a>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </li>--}}
-
-    <!-- Nav Item - Charts -->
-{{--    <li class="nav-item">--}}
-{{--        <a class="nav-link" href="charts.html">--}}
-{{--            <i class="fas fa-fw fa-chart-area"></i>--}}
-{{--            <span>Charts</span></a>--}}
-{{--    </li>--}}
-
-    <!-- Nav Item - Tables -->
-{{--    <li class="nav-item">--}}
-{{--        <a class="nav-link" href="tables.html">--}}
-{{--            <i class="fas fa-fw fa-table"></i>--}}
-{{--            <span>Tables</span></a>--}}
-{{--    </li>--}}
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
