7bbb9c5d4e4a892847e00c45110c8a74b97cfe0e
AniSargsyan97 <ani97sargsyan@gmail.com>
2025-05-06 15:33:05 +0400

    Creating menu items list

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,123 +1,42 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     {{--    @include('admin.layout.menu_items')--}}
-    @php
-        $menu_items = [
-            [
-                'url' => 'admin_list',
-                'name' => 'admin_list',
-                'label' => 'Ադմիններ',
-                'params' => null,
-                'role' => 3,
-                'icon' => 'fas fa-users'
-            ],
-            [
-                'url' => 'admin_menu_list',
-                'name' => 'menu_list',
-                'label' => 'Մենյու',
-                'params' => null,
-                'role' => 2,
-                'icon' => 'fas fa-bars'
-            ],
-             [
-                'url' => 'product_list',
-                'name' => 'product_list',
-                'label' => 'Ապրանք',
-                'params' => [0],
-                'role' => 3,
-                'icon' => 'fas fa-couch'
-            ],
-            [
-                'url' => 'product_type',
-                'name' => 'prod_type_list',
-                'label' => 'Ապրանքի տիպ',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-clipboard'
-            ],
-            [
-                'url' => 'companies_list',
-                'name' => 'company_list',
-                'label' => 'Կազմակերպություններ',
-                'params' => null,
-                'role' => 2,
-                'icon' => 'fas fa-briefcase'
-            ],
-            [
-                'url' => 'shop_list',
-                'name' => 'market_list',
-                'label' => 'Խանութներ',
-                'params' => [0],
-                'role' => 3,
-                'icon' => 'fas fa-store'
-            ],
-            [
-                'url' => 'admin_category_list',
-                'name' => 'category_list',
-                'label' => 'Կատեգորիաներ',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-layer-group'
-            ],
-            [
-                'url' => 'product_category',
-                'name' => 'prod_category_list',
-                'label' => 'Պրոդուկտի կատեգորիաներ',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-file'
+{{--    @php--}}
+{{--        $menu_items = [--}}
 
-            ],
-            [
-                'url' => 'contract_request_list',
-                'name' => 'contract_list',
-                'label' => 'Պայմանագրի հայտեր',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-handshake'
-            ],
-             [
-                'url' => 'payment',
-                'name' => 'payment_list',
-                'label' => 'Վճարումներ',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-wallet'
-            ],
-            [
-                'url' => 'discount_by_payment',
-                'name' => 'discount_by_payment',
-                'label' => 'Վճարումների զեղչեր',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-percent'
-            ],
-            [
-                'url' => 'discount_by_shop',
-                'name' => 'discount_by_shop',
-                'label' => 'Զեղչ ըստ խանութների',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-percent'
-            ],
-            [
-                'url' => 'price_by_product',
-                'name' => 'price_by_product',
-                'label' => 'Գին ըստ պրոդուկտի',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-tags'
-            ],
-              [
-                'url' => 'not_create',
-                'name' => 'notification',
-                'label' => 'Հաղորդագրություն',
-                'params' => null,
-                'role' => 1,
-                'icon' => 'fas fa-envelope'
-            ]
-    ];
-        $page = Session::get('page');
-    @endphp
+{{--             [--}}
+{{--                'url' => 'product_list',--}}
+{{--                'name' => 'product_list',--}}
+{{--                'label' => 'Products',--}}
+{{--                'params' => [0],--}}
+{{--                'icon' => 'fas fa-couch'--}}
+{{--            ],--}}
+{{--            [--}}
+{{--                'url' => 'product_type',--}}
+{{--                'name' => 'prod_type_list',--}}
+{{--                'label' => 'Orders',--}}
+{{--                'params' => null,--}}
+{{--                'icon' => 'fas fa-clipboard'--}}
+{{--            ],--}}
+{{--            [--}}
+{{--                'url' => 'companies_list',--}}
+{{--                'name' => 'company_list',--}}
+{{--                'label' => 'Order_items',--}}
+{{--                'params' => null,--}}
+{{--                'role' => 2,--}}
+{{--                'icon' => 'fas fa-briefcase'--}}
+{{--            ],--}}
+{{--            [--}}
+{{--                'url' => 'shop_list',--}}
+{{--                'name' => 'market_list',--}}
+{{--                'label' => 'Warehouses',--}}
+{{--                'params' => [0],--}}
+{{--                'role' => 3,--}}
+{{--                'icon' => 'fas fa-store'--}}
+{{--            ],--}}
+
+{{--    ];--}}
+{{--        $page = Session::get('page');--}}
+{{--    @endphp--}}
 
     <a class="sidebar-brand d-flex align-items-center justify-content-center" style="line-height: 1.5"
        href="">
@@ -128,46 +47,61 @@
     </a>
 
     <hr class="sidebar-divider my-0">
+        <li class="nav-item menu_active_item">
+            <a class="nav-link"
+               href="#">
+                <i class="fas fa-store"></i>
+                <span>Products</span>
+            </a>
+        </li>
+        <li class="nav-item menu_active_item">
+            <a class="nav-link"
+               href="#">
+                <i class="fas fa-clipboard"></i>
+                <span>Orders</span>
+            </a>
+        </li>
+        <li class="nav-item menu_active_item">
+            <a class="nav-link"
+               href="#">
+                <i class="fas fa-file"></i>
+                <span>Order_items</span>
+            </a>
+        </li>
+        <li class="nav-item menu_active_item">
+            <a class="nav-link"
+               href="#">
+                <i class="fas fa-warehouse"></i>
+                <span>Warehouses</span>
+            </a>
+        </li>
+        <li class="nav-item menu_active_item">
+            <a class="nav-link"
+               href="#">
+                <i class="fas fa-business-time"></i>
+                <span>Stocks</span>
+            </a>
+        </li>
 
-{{--    @foreach($menu_items as $item)--}}
-{{--        @if(Auth::guard('admin')->user()->role_id <= $item['role'] )--}}
-{{--            <li class="nav-item @if($page == $item['name']) menu_active_item @endif">--}}
-{{--                <a class="nav-link"--}}
-{{--                   href="{{ (!is_null($item['params'])) ? route($item['url'], $item['params']) : route($item['url']) }}">--}}
-{{--                    <i class="{{$item['icon']}}"></i>--}}
-{{--                    <span>{{$item['label']}}</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--        @endif--}}
-{{--    @endforeach--}}
 
-{{--    @if(Session::get('page')=='update-password'||Session::get('page')=='update-admin-details')--}}
-{{--        @php--}}
-{{--            $active = "active";--}}
-{{--        @endphp--}}
-{{--    @else--}}
-{{--        @php--}}
-{{--            $active = "";--}}
-{{--        @endphp--}}
-{{--    @endif--}}
-{{--    <li class="nav-item">--}}
-{{--        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"--}}
-{{--           aria-expanded="true" aria-controls="collapseTwo">--}}
-{{--            <i class="fas fa-fw fa-cog"></i>--}}
-{{--            <span>Կարգավորումներ</span>--}}
-{{--        </a>--}}
-{{--        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">--}}
-{{--            <div class="bg-white py-2 collapse-inner rounded">--}}
-{{--                <h6 class="collapse-header">Հիմնական Կարգավորումներ</h6>--}}
-{{--                <a class="collapse-item nav-link set_class" href="{{url('admin/update-password')}}">Փոխել գաղտնաբառը</a>--}}
-{{--                <a class="collapse-item nav-link set_class" href="{{url('admin/update-admin-details')}}">Ադմինի--}}
-{{--                    տվյալների <br>թարմացում</a>--}}
+{{--        <li class="nav-item">--}}
+{{--            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"--}}
+{{--               aria-expanded="true" aria-controls="collapseTwo">--}}
+{{--                <i class="fas fa-fw fa-cog"></i>--}}
+{{--                <span>Settings</span>--}}
+{{--            </a>--}}
+{{--            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">--}}
+{{--                <div class="bg-white py-2 collapse-inner rounded">--}}
+{{--                    <h6 class="collapse-header">Basic Settings</h6>--}}
+{{--                    <a class="collapse-item nav-link set_class" href="{{url('admin/update-password')}}">Change password</a>--}}
+{{--                    --}}{{--                <a class="collapse-item nav-link set_class" href="{{url('admin/update-admin-details')}}">Ադմինի--}}
+{{--                    --}}{{--                    տվյալների <br>թարմացում</a>--}}
+{{--                </div>--}}
 {{--            </div>--}}
-{{--        </div>--}}
 
-{{--    </li>--}}
+{{--        </li>--}}
 
-    <!-- Nav Item - Utilities Collapse Menu -->
+{{--    <!-- Nav Item - Utilities Collapse Menu -->--}}
 {{--    <li class="nav-item">--}}
 {{--        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"--}}
 {{--           aria-expanded="true" aria-controls="collapseUtilities">--}}
